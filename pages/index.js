5a81a1128b9e27ee64138c0e8ad8550eefc7613a
Vadim <_s_e_t__@ukr.net>
2025-01-04 12:20:08 +0200

    add Home page

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import MeetupList from "@/components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/New_York_Midtown_Skyline_at_night_-_Jan_2006_edit1.jpg/1280px-New_York_Midtown_Skyline_at_night_-_Jan_2006_edit1.jpg?uselang=ru",
+    address: "Some address",
+    description: "This is a first meetup",
+  },
+  {
+    id: "m2",
+    title: "A Second Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/New_York_Midtown_Skyline_at_night_-_Jan_2006_edit1.jpg/1280px-New_York_Midtown_Skyline_at_night_-_Jan_2006_edit1.jpg?uselang=ru",
+    address: "Some address",
+    description: "This is a second meetup",
+  },
+];
+
+const HomePage = () => {
+    return <MeetupList meetups={DUMMY_MEETUPS } />
+}
+
+export default HomePage;
