787d49289944d1d53948d2347631d2d44ae3b0b0
Vadim <_s_e_t__@ukr.net>
2025-01-06 12:21:16 +0200

    add MeetupDetail component

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "@/components/meetups/MeetupDetail";
+
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/New_York_Midtown_Skyline_at_night_-_Jan_2006_edit1.jpg/1280px-New_York_Midtown_Skyline_at_night_-_Jan_2006_edit1.jpg?uselang=ru"
+      title="The First Meetup"
+      address="some address"
+      description="some description"
+    />
+  );
+};
+
+export default MeetupDetails;
